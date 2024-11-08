13ba49ce9490ca34729826450d73d442d1c8edef
FlorianTschimben <florian.tschimben@tfobz.net>
2024-11-08 21:52:23 +0100

    Added mysql functionality and moved Database module

--- Database/src/DatabaseManager.java
+++ Database/src/DatabaseManager.java
@@ -6,13 +6,13 @@ import java.sql.*;
  * @since 2024-11-08
  */
 public class DatabaseManager {
-	static Connection c = null;
+	private static Connection c = null;
 
 	public static void initialize(Connection c) throws SQLException {
 		Statement fahrzeugtypen = c.createStatement();
 		String fahrzeugtypenCreate = """
 			CREATE TABLE fahrzeugtypen(
-			\tfnr INT PRIMARY KEY,
+				fnr INT PRIMARY KEY,
 			    fname VARCHAR(4) NOT NULL,
 			    fliege BOOLEAN NOT NULL,
 			    fstuhl INT NOT NULL,
@@ -20,27 +20,28 @@ public class DatabaseManager {
 			    fmawk INT NOT NULL,
 			    fmaximalpassagiere INT NOT NULL
 			)""";
-		fahrzeugtypen.executeQuery(fahrzeugtypenCreate);
+		fahrzeugtypen.execute(fahrzeugtypenCreate);
 
 		Statement transport = c.createStatement();
 		String transportCreate = """
 			CREATE TABLE transport(
-			\ttnr VARCHAR(10) PRIMARY KEY,
-			    tdatum DATE NOT NULL,
-			    tstart TIME NOT NULL,
-			    tende TIME NOT NULL,
-			    tvonort VARCHAR(100) NOT NULL,
-			    tvonstrasse VARCHAR(100) NOT NULL,
-			    tbisort VARCHAR(100) NOT NULL,
-			    tbisstrasse VARCHAR(100) NOT NULL,
-			    tbezugnr VARCHAR(10),
-			    tkmtotale INT NOT NULL,
-			    fnr INT NOT NULL,
-			    tsektionsoirt VARCHAR(100),
-			    FOREIGN KEY (fnr) REFERENCES fahrzeugtypen(fnr)
-			    \tON UPDATE CASCADE ON DELETE RESTRICT
+					tnr VARCHAR(10) PRIMARY KEY,
+				    tdatum DATE NOT NULL,
+				    tstart TIME NOT NULL,
+				    tende TIME NOT NULL,
+				    tvonort VARCHAR(100) NOT NULL,
+				    tvonstrasse VARCHAR(100) NOT NULL,
+				    tbisort VARCHAR(100) NOT NULL,
+				    tbisstrasse VARCHAR(100) NOT NULL,
+				    tart ENUM("KANN GEHEN", "STUHL", "LIEGE", "KEIN PATIENT", "EIGENER ROLLSTUHL") NOT NULL,
+				    tbezugnr VARCHAR(10),
+				    tkmtotale INT NOT NULL,
+				    fnr INT NOT NULL,
+				    tsektionsoirt VARCHAR(100),
+				    FOREIGN KEY (fnr) REFERENCES fahrzeugtypen(fnr)
+			    	ON UPDATE CASCADE ON DELETE RESTRICT
 			)""";
-		transport.executeQuery(transportCreate);
+		transport.execute(transportCreate);
 	}
 
 	public static Connection connect(String url, String username, String password) throws SQLException {
