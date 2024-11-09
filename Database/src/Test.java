135bf26176f93ce607dc7a05ac492a4fd00452f9
FlorianTschimben <florian.tschimben@tfobz.net>
2024-11-09 05:10:52 +0100

    Isch schun zu long her

--- Database/src/Test.java
+++ Database/src/Test.java
@@ -10,10 +10,9 @@ public class Test {
 		try {
 			Connection c = DatabaseManager.connect("jdbc:mysql://localhost/hackathon", "root", "");
 			//DatabaseManager.initialize(c);
-			Transport t = new Transport("2","2010-01-01", "02:45:00", "03:05:00", "Nals",
-				"Andreas-Hofer-Straße 7", "Meran", "Meranrgasse 8", Transport.TransportArt.STUHL ,null, 30,
-				2, "Meran");
-			DatabaseManager.insert(t.generateInsertQuery());
+			ResultSet rs = DatabaseManager.getDuplicateTragetAndTime(c);
+			rs.next();
+
 		}
 		catch (Exception e) {
 			throw new RuntimeException(e);
